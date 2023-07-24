f3d79d8f4607d9ad7195c262eed6898fc75b5186
S. A. Ortega <angelus5892@gmail.com>
2023-07-24 18:53:14 +0200

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    REQUIREMENTS = f.read().splitlines()
+
+setup(name='squwals',
+      version='1.0',
+      description='Szegedy Quantum Walk Simulator',
+      author='Sergio A. Ortega and Miguel A. Martin-Delgado',
+      author_email='sortega5892@gmail.com',
+      url='https://github.com/OrtegaSA/TSNAN',
+      license='Apache 2.0',
+      # classifiers=[
+      #   "Environment :: Console",
+      #   "License :: OSI Approved :: Apache Software License",
+      #   "Intended Audience :: Developers",
+      #   "Intended Audience :: Science/Research",
+      #   "Operating System :: Microsoft :: Windows",
+      #   "Operating System :: MacOS",
+      #   "Operating System :: POSIX :: Linux",
+      #   "Programming Language :: Python :: 3 :: Only",
+      #   "Topic :: Scientific/Engineering",
+      # ],
+      install_requires=REQUIREMENTS,
+      python_requires=">=3.0",
+      include_package_data=True,
+      packages=find_packages(),
+     )
---
+++ squwals/__init__.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+def f(x):
+    return x+2
