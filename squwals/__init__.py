a5bed3d8a23e48a65d83a626ea0ca3e50253b780
S. A. Ortega <angelus5892@gmail.com>
2024-12-01 03:16:53 +0100

    v2.0

--- squwals/__init__.py
+++ squwals/__init__.py
@@ -18,7 +18,7 @@ This is a package with utilities to simulate Szegedy's quantum walk in an
 efficient manner, saving time and memory resources.
 """
 
-__version__ = '1.2'
+__version__ = '2.0'
 
 from squwals.operators import Unitary, Swap, Oracle, Reflection, Measurement
 from squwals.operators import SingleUnitary, DoubleUnitary
